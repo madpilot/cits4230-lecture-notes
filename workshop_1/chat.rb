df5d4da2e01123cf6aeb49da10854263fcecdef4
Myles Eftos <myles@madpilot.com.au>
2010-09-23 13:29:06 +0800

    Adding some comments

--- workshop_1/chat.rb
+++ workshop_1/chat.rb
@@ -1,16 +1,24 @@
 class Chat
+  # Initialize the class
+  #
+  # Parameters
+  #   name: a User name
+  #   log_file: The path to the file that will hold the chat log (default: chat.dat)
   def initialize(name, log_file = 'chat.dat')
     @name = name
     @line = 0
     @log_file = log_file
   end
 
+  # Prints a command prompt and waits for an input from the user. Returns the input, without the trailing new line.
   def prompt
     print build_prompt_for_display(@name, @line)
     input = gets.strip
     input
   end
 
+  # Displays the command prompt and saves everything to the chat file.
+  # the method will yield the input to the supplied block
   def log
     File.open(@log_file, 'a+') do |fh|
       while input = self.prompt
@@ -25,14 +33,18 @@ class Chat
     end
   end
 
+  # serialise the message ready for saving
   def build_prompt_for_log(name, time, message)
     "#{name}:#{time.to_i}:#{message}"
   end
 
+  # Returns a nicer string version of the message
   def build_prompt_for_display(name, line, message = nil)
     "#{name.downcase}:#{line}>" + (message ? " #{message}" : '')
   end
 
+  # Yields each line of the current chat log
+  # The block must take three arguments: name, time and message
   def get_log
     File.open(@log_file, 'r') do |fh|
       fh.read.split("\n").each do |line|
